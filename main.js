01f5e8ebfe4f4c052c4c9b29694047fb7222f278
sevcenkomaksim428-ux <sevcenkomaksim428@gmail.com>
2025-09-08 12:43:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// const tabItem=document.querySelectorAll('.tabs_btn_item');
+// const tabContent=document.querySelectorAll('.tabs_content_item');
+// tabItem.forEach(function(element){
+//     element.addEventListener('click',open);
+// });
+//     function open(evt){
+//     const tabTarget=evt.currentTarget;
+//     const button= tabTarget.dataset.button;
+    
+//     tabItem.forEach(function(item){
+//         item.classList.remove('tabs_btn_item_active');
+//     });
+
+//     tabContent.forEach(function(item){
+//         item.classList.remove('tabs_content_item_active');
+//     });
+    
+//     tabTarget.classList.add('tabs_btn_item_active');
+//     document.querySelector(#${button}).classList.add('tabs_content_item_active');
+    
+// };
